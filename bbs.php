89bca8555d5fd604f88d8399e0454b2f77e92cc1
ippei.ohshiro <castle.bbstyle@gmail.com>
2020-04-06 23:47:47 +0900

    保存処理 入力内容バリデーション

--- bbs.php
+++ bbs.php
@@ -5,8 +5,44 @@ if(!$link){
     die('データベースに接続できません:' . mysqli_error());
 }
 
-mysqli_select_db('online_bbs', $link);
+mysqli_select_db($link, 'online_bbs');
 
+$errors = array();
+
+//POSTなら保存処理実行
+if($_SERVER['REQUEST_METHOD'] === 'post'){
+    //名前が正しいかチェック
+    $name = null;
+    if(!isset($_POST['name']) || !strlen($_POST['name'])){
+        $errors['name'] = '名前を正しく入力してください';
+    } elseif(strlen($_POST['name']) > 40) {
+        $errors['name'] = '名前は４０文字以内で入力してください';
+    } else {
+        $name = $_POST['name'];
+    }
+
+    //ひとことが正しく入力されているかチェック
+    $comment = null;
+    if(!isset($_POST['comment']) || !strlen($_POST['comment'])) {
+        $errors['comment'] = 'ひとことを入力してください';
+    } elseif(strlen($_POST['comment']) > 200) {
+        $errors['comment'] = 'ひとことは200文字以内で入力して下さい';
+    } else {
+        $comment = $_POST['comment'];
+    }
+
+    //エラーがなければ保存
+    if(count($errors) === 0) {
+        //保存用のSQL分作成
+        $sql = "INSERT INTO post (name, comment,  created_at) VALUES ('"
+            . mysqli_real_escape_string($name) . "', '"
+            . mysqli_real_escape_string($comment) . "', '"
+            . date('Y-m-d H:i:s') . "')"; 
+    }
+
+    //保存する
+    mysql_query($sql, $link);
+}
 ?>
 
 
@@ -25,5 +61,6 @@ mysqli_select_db('online_bbs', $link);
         ひとこと: <input type="text" name="comment" size="60"><br>
         <input type="submit" name="submit" value="送信">
     </form>
+    
 </body>
 </html>
