d2af58c2a3bbf4063b1c80ffcbb9819a8fb092fb
ippei.ohshiro <castle.bbstyle@gmail.com>
2020-04-06 22:14:40 +0900

    フォーム作成

---
+++ bbs.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ひとこと掲示板</title>
+</head>
+<body>
+    <h1>ひとこと掲示板</h1>
+
+    <form action="bbs.php" method="post">
+        名前: <input type="text" name="name"><br>
+        ひとこと: <input type="text" name="comment" size="60"><br>
+        <input type="submit" name="submit" value="送信">
+    </form>
+</body>
+</html>
